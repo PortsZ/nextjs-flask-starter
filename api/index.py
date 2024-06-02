0713f279c2e8f2bd04969a623bc564e85106d061
= <=>
2024-06-02 22:34:23 +0200

    Python test vercel.json

--- api/index.py
+++ api/index.py
@@ -1,6 +1,31 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+# Set the environment variable
+os.environ["FLASK_ENV"] = "development"
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+
+# Main block for local development
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# # This ensures the app is discoverable by Flask when run as a module
+# if __name__ != "__main__":
+#     from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+#     def create_app():
+#         return app
+
+#     app = DispatcherMiddleware(app, {
+#         '/api/python': create_app()
+#     })
