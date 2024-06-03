86ccb49d02f1ed9580401ff4c57e3abc93d1143f
= <=>
2024-06-03 16:39:58 +0200

    Vercel test python 19

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,20 @@ import axios from "axios";
 
 export default function Home() {
   const [data, setData] = useState<any>(<></>);
+  const [data2, setData2] = useState<any>(<></>);
 
   useEffect(() => {
-    const res = axios.get("/api/python")
-      .then((res) => setData(res.data))
+    axios.get("/api/python").then((res) => setData(res.data));
 
-      console.log(data)
+    axios.get("/api/test").then((res) => setData2(res.data));
+
+    console.log(data);
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white font-bold text-3xl">
-        <div dangerouslySetInnerHTML={{ __html: data }} />
+      <div dangerouslySetInnerHTML={{ __html: data }} />
+      <div dangerouslySetInnerHTML={{ __html: data2 }} />
     </main>
   );
 }
