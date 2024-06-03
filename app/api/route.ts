4a0ee961c1d8f3c99b95eba9820e3f5f8ca85a07
= <=>
2024-06-03 16:01:54 +0200

    Vercel test python 6

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";	
 
 export async function GET() {
-   const res = await axios.get("http://127.0.0.1:5328/api/python")
+   const res = await axios.get("/api/python")
 
    return new Response(res.data)
 }
