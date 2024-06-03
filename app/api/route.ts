79d540609cf5feefb3236da7bd75b9800ec36677
= <=>
2024-06-03 16:13:11 +0200

    Vercel test python 8

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,7 @@
 import axios from "axios";	
 
 export async function GET() {
-   const res = await axios.get("/api/python")
+   const res = await axios.get("api/api/python")
 
    return new Response(res.data)
 }
